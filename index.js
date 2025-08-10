ea5ad1135ae38a6513f7c30083556ad49f62f51b
Sa-Tori <tori.kalipso@bk.ru>
2025-08-10 23:22:47 +0300

    the deers

--- index.js
+++ index.js
@@ -6,8 +6,17 @@ const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
+const axios = require('axios');
+const SELF_URL = 'https://твой-сабдомен.onrender.com';
+
+setInterval(() => {
+    axios.get(SELF_URL)
+        .then(() => console.log(`[KeepAlive] Pinged ${SELF_URL}`))
+        .catch(err => console.error(`[KeepAlive] Error: ${err.message}`));
+}, 14 * 60 * 1000); // каждые 14 минут
+
 client.once('ready', () => {
-    console.log("DISCORD_TOKEN:", process.env.DISCORD_TOKEN);
+    //console.log("DISCORD_TOKEN:", process.env.DISCORD_TOKEN);
     console.log(`✅ Logged in as ${client.user.tag}`);
 });
 
