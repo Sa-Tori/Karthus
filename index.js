85970e959f32b1d69d6e62b6cc7f902f9663fd24
Sa-Tori <tori.kalipso@bk.ru>
2026-03-01 16:56:27 +0300

    фикс 1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 // ─── Фейковый сервер для Render ──────────────────────────────────────
 require('./server'); // Express
 
-const SELF_URL = 'https://karthus.onrender.com';
+const SELF_URL = 'https://karthus-something.koyeb.app';
 
 setInterval(() => {
     axios.get(SELF_URL)
